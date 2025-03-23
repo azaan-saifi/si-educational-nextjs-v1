e60c3074a3db2f8e454d62601ebf293c94a2f34f
Azaan saifi <saifiazaan1@gmail.com>
2025-03-23 15:19:55 +0530

    change image

--- app/components/Banner/Banner.tsx
+++ app/components/Banner/Banner.tsx
@@ -25,13 +25,13 @@ const Banner = () => {
                   width={38}
                   height={38}
                 />
-                <Image
+                {/* <Image
                   className="inline-block rounded-full ring-2 ring-white"
                   src="https://images.unsplash.com/photo-1550525811-e5869dd03032?ixlib=rb-1.2.1&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
                   alt=""
                   width={38}
                   height={38}
-                />
+                /> */}
                 <Image
                   className="inline-block rounded-full ring-2 ring-white"
                   src="https://images.unsplash.com/photo-1500648767791-00dcc994a43e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2.25&w=256&h=256&q=80"
